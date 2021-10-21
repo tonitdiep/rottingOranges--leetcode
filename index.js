aaf90770a9f05bc29590242df4f13ce4615a42d1
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-21 03:01:54 +0000

    Leetcode 994 question

---
+++ index.js
@@ -0,0 +1,31 @@
+Leetcode # 994 Rotting Oranges (medium)
+
+You are given an m x n grid where each cell can have one of three values:
+
+0 representing an empty cell,
+1 representing a fresh orange, or
+2 representing a rotten orange.
+Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+
+Example 1: 
+Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+Output: 4
+
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 10
+grid[i][j] is 0, 1, or 2.
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function(grid) {
+    // let emptyCell = 0;
+    // let freshO = 1;
+    // let rottenO = 2;
+    return -1
+};
